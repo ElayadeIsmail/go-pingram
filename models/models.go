5f05740efb08407b189d798fde51b2b3c24c3d75
Elayade ismail <elayadeismail@gmail.com>
2021-10-28 17:44:52 +0100

    update field naming

--- models/models.go
+++ models/models.go
@@ -1,21 +1,29 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Username string `gorm:"unique" json:"username" validate:"required,min=3,max=10"`
-	Email    string `gorm:"unique" json:"email" validate:"required,email"`
-	Password string `json:"-" validate:"required,min=6,max=22"`
-	Posts    []Post `json:"posts"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Username  string         `gorm:"unique" json:"username" validate:"required,min=3,max=10"`
+	Email     string         `gorm:"unique" json:"email" validate:"required,email"`
+	Password  string         `json:"-" validate:"required,min=6,max=22"`
+	Posts     []Post         `json:"posts"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
 
 type Post struct {
-	gorm.Model
-	Text     string `json:"text" validate:"required" `
-	ImageUrl string `json:"imageUrl"`
-	UserID   uint   `json:"userId"`
-	User     User   `json:"user"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Text      string         `json:"text" validate:"required" `
+	ImageUrl  string         `json:"imageUrl"`
+	UserID    uint           `json:"userId"`
+	User      User           `json:"user"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
